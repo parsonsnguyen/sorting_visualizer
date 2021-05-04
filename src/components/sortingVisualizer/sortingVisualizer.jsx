c06faef3014afe7589af3e3a32bef45c61d1c044
Parsons Nguyen <parsons.nguyen@nexcel.vn>
2021-05-04 16:05:47 +0700

    0001 Update UI - Part 1.

--- src/components/sortingVisualizer/sortingVisualizer.jsx
+++ src/components/sortingVisualizer/sortingVisualizer.jsx
@@ -1,127 +1,132 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { Component } from "react";
+import { sortingAlgorithms } from "../utils";
 import "./sortingVisualizer.css";
-import visualize from "../helpers";
-import Slider from "@material-ui/core/Slider";
-
-const SortingVisualizer = () => {
-  //const [brightness, setBrightness] = useState(0);
-  const [array, setArray] = useState([]);
-  const [speed, setSpeed] = useState(5);
-  const [size, setSize] = useState(50);
-  const [buttonDisabled, setDisabled] = useState(false);
-  const [generateDisabled, toggleGenerate] = useReducer(
-    (disabled) => !disabled,
-    false
-  );
-
-  const generateArrayVals = (max, min, length) => {
-    let arr = [];
-    for (let i = 0; i < length; i++) {
-      arr[i] = Math.floor(Math.random() * (max - min + 1) + min);
+import {
+    Button,
+    TextField,
+    InputLabel,
+    Select,
+    MenuItem,
+    Slider,
+    Grid
+  } from '@material-ui/core';
+export default class SortingVisualizer extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        selectedSortingType: props.selectedSortingType || 'Quick_Sort',
+        arraySize: 20,
+        delaySpeed: 500
+      };
+      this.arrays = [10,200,500];
     }
-    const container = document.getElementById("array-container");
-    Array.from(container.children).forEach(
-      (e) => (e.style.backgroundColor = "OrangeRed")
-    );
-    return setArray(arr);
+    onSelectedSortingTypeChange = (event) => {
+      this.setState({ selectedSortingType: event.target.value });
+    }
+    
+    onTxtArraySizeChange = (event) => {
+        this.setState({ arraySize: event.target.value });
+    }
+    onSliderSizeChange = (e, val) => {
+        //if (val === size) return;
+        //setSize(val);
+        //generateArrayVals(1000, 5, val);
+        console.log(val);
+        this.setState({ arraySize: val });
+    };
+
+    onTxtDelaySpeedChange = (event) => {
+        this.setState({ delaySpeed: event.target.value });
+    }
+    onSliderDelaySpeedChange = (e, val) => {
+        //if (val === size) return;
+        //setSize(val);
+        //generateArrayVals(1000, 5, val);
+        console.log(val);
+        this.setState({ delaySpeed: val });
+    };
+
+    render() {
+      const { selectedSortingType, arraySize, delaySpeed } = this.state;
+      return (
+        <>
+            <div className="makeStyles-root-1">
+                <Grid container spacing={3}>
+                    <Grid container item xs={2}></Grid>
+                    <Grid container item xs={8}>
+                        <Grid item xs={3}>
+                            <TextField 
+                                onChange={this.onTxtArraySizeChange}
+                                id="txt-array-size" 
+                                type="number" 
+                                label="Input array size"  
+                                value={arraySize}
+                                inputProps={{min: 10, style: { textAlign: 'center' }}}/> 
+                            <div className="sliders">
+                                <Slider /*key={`slider-${arraySize}`}*/  min={10} max={500} step={10} defaultValue={arraySize}
+                                        onChange={this.onSliderSizeChange}
+                                        //disabled={buttonDisabled}
+                                        marks={true}
+                                        valueLabelDisplay="auto"
+                                />
+                            </div>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <TextField 
+                                onChange={this.onTxtDelaySpeedChange}
+                                id="txt-delay-speed" 
+                                type="number" 
+                                label="Input delay speed" 
+                                value={delaySpeed} 
+                                inputProps={{min: 100, style: { textAlign: 'center' }}}/>
+                            <div className="sliders">
+                                <Slider /*key={`slider-${delaySpeed}`}*/ min={100} max={2000} step={100} defaultValue={delaySpeed}
+                                        onChange={this.onSliderDelaySpeedChange}
+                                        //disabled={buttonDisabled}
+                                        marks={true}
+                                        valueLabelDisplay="auto"
+                                />
+                            </div> 
+                        </Grid>
+                        <Grid item xs={3}>
+                            <InputLabel id="sortingType">Please select sorting type</InputLabel>
+                            <Select
+                                labelId="sortingType"
+                                id="sortingTypeSelect"
+                                value={selectedSortingType}
+                                onChange={this.onSelectedSortingTypeChange}
+                            >
+                                {sortingAlgorithms.map((item,index) => <MenuItem value={item.type}>{item.name}</MenuItem>)}
+                            </Select>
+                        </Grid>
+                       
+                        <Grid item xs={3}>
+                            <Button variant="outlined">Generate new Array</Button>
+                            <Button variant="outlined" color="primary">Sort</Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={2}></Grid>
+                </Grid>
+            </div>
+            <hr></hr>
+            <div id="array-container">
+                {this.arrays.map((value, index) => (
+                <div
+                    className="array-bar"
+                    key={index}
+                    style={
+                        { height: `${value / 13}vh`,
+                         width: `${(1 / this.arrays.length) * 1000}px`
+                        }}
+                ></div>
+                ))}
+            </div>
+        </>
+       );
+    }
+  }
+  
+  SortingVisualizer.propTypes = {
   };
-  useEffect(() => generateArrayVals(1000, 5, size), [size]);
-
-  const algorithm = (type) => {
-    setDisabled(true);
-    toggleGenerate();
-    visualize(type, array, speed, toggleGenerate);
-  };
-
-  const handleSize = (e, val) => {
-    if (val === size) return;
-    setSize(val);
-    generateArrayVals(1000, 5, val);
-  };
-
-  const handleSpeed = (e, val) => {
-    if (val === speed) return;
-    const newSpeed = Math.abs(10 - (val - 0.5));
-    setSpeed(newSpeed);
-    //setBrightness(speed * 10 + -50);
-  };
-
-  const handleGenerate = () => {
-    generateArrayVals(1000, 5, size);
-    setDisabled(false);
-  };
-
-  return (
-    <>
-      <div>
-        <h1>Filter</h1>
-        <button
-          variant="contained"
-          color="primary"
-          onClick={handleGenerate}
-          disabled={generateDisabled}
-        >
-          Generate New Array
-        </button>
-        <button
-          onClick={() => algorithm("insertion")}
-          disabled={buttonDisabled}
-        >
-          Insertion Sort (Slowest)
-        </button>
-        <button onClick={() => algorithm("bubble")} disabled={buttonDisabled}>
-          Bubble Sort (Slow)
-        </button>
-        <button
-          onClick={() => algorithm("selection")}
-          disabled={buttonDisabled}
-        >
-          Selection Sort (Fast)
-        </button>
-      </div>
-      <div className="sliders">
-        Size:{" "}
-        <Slider
-          min={10}
-          max={150}
-          step={10}
-          defaultValue={50}
-          onChange={handleSize}
-          disabled={buttonDisabled}
-          marks={true}
-          valueLabelDisplay="auto"
-        />
-        Speed:
-        <Slider
-          className="slider"
-          style={{ display: "block" }}
-          color="secondary"
-          step={0.5}
-          min={1}
-          max={10}
-          marks={true}
-          defaultValue={5}
-          onChange={handleSpeed}
-          disabled={buttonDisabled}
-          valueLabelDisplay="auto"
-        />
-      </div>
-      <hr />
-      <div id="array-container">
-        {array.map((value, index) => (
-          <div
-            className="array-bar"
-            key={index}
-            style={{
-              height: `${value / 13}vh`,
-              width: `${(1 / array.length) * 1000}px`,
-              //filter: `hue-rotate(${brightness}deg)`,
-            }}
-          ></div>
-        ))}
-      </div>
-    </>
-  );
-};
-
-export default SortingVisualizer;
+  
---
+++ src/components/utils.js
@@ -0,0 +1,75 @@
+export const sortingAlgorithms = 
+[{
+    "type": "Selection_Sort",
+    "name": "Selection Sort",
+    "timeComplexity": {
+        "best": "&Theta;(n<sup>2</sup>)",
+        "average": "&Theta;(n<sup>2</sup>)",
+        "worst": "&Theta;(n<sup>2</sup>)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(1)"
+    }
+},
+{
+    "type": "Bubble_Sort",
+    "name": "Bubble Sort",
+    "timeComplexity": {
+        "best": "&Theta;(n)",
+        "average": "&Theta;(n<sup>2</sup>)",
+        "worst": "&Theta;(n<sup>2</sup>)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(1)"
+    }
+},
+{
+    "type": "Insertion_Sort",
+    "name": "Insertion Sort",
+    "timeComplexity": {
+        "best": "&Theta;(n)",
+        "average": "&Theta;(n<sup>2</sup>)",
+        "worst": "&Theta;(n<sup>2</sup>)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(1)"
+    }
+},
+{
+    "type": "Quick_Sort",
+    "name": "Quick Sort",
+    "timeComplexity": {
+        "best": "&Theta;(nlogn)",
+        "average": "&Theta;(nlogn)",
+        "worst": "&Theta;(n<sup>2</sup>)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(logn)"
+    }
+},
+{
+    "type": "Merge_Sort",
+    "name": "Merge Sort",
+    "timeComplexity": {
+        "best": "&Theta;(nlogn)",
+        "average": "&Theta;(nlogn)",
+        "worst": "&Theta;(nlogn)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(n)"
+    }
+},
+
+{
+    "type": "Heap_Sort",
+    "name": "Heap Sort",
+    "timeComplexity": {
+        "best": "&Theta;(nlogn)",
+        "average": "&Theta;(nlogn)",
+        "worst": "&Theta;(nlogn)"
+    },
+    "memoryComplexity": {
+        "worst": "&Theta;(1)"
+    }
+}
+];
