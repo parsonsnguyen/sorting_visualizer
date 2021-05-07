256e11dc0ea9ecb5a523139515733ebbe2b73d42
Parsons Nguyen <parsons.nguyen@nexcel.vn>
2021-05-07 13:54:41 +0700

    0001 Integrate selection sort.

--- src/components/algorithms/selectionSortAsync.js
+++ src/components/algorithms/selectionSortAsync.js
@@ -1,6 +1,30 @@
-import {swapArrItem} from '../utils'
-import {prepareSwappingStep, swap, endSwappingStep} from '../visualizer'
+import {swapArrItems} from '../utils'
+import {COLORs} from '../constants'
+import {changeColor, swapPosition_of_2Values, markColor_of_NormalValues} from '../visualizer'
 
 export default async function selectionSortAsync(array) {
-  console.log('update later');
+  for (let i = 0; i < array.length; i++) {
+      let minIndex = i, minValue = array[i];
+      await changeColor(minValue, COLORs.CandidateValue);
+
+      for (let j = i + 1; j < array.length; j++) {
+          const currentValue = array[j];
+          await changeColor(currentValue, COLORs.ComparingValue);
+          if (currentValue < minValue) {
+              await changeColor(minValue, COLORs.NormalValue);
+              await changeColor(currentValue, COLORs.CandidateValue);
+              minIndex = j;
+              minValue = currentValue;
+          } else {
+              await changeColor(currentValue, COLORs.NormalValue);
+          }
+      }
+
+      if (minIndex !== i) {
+          await swapPosition_of_2Values(minValue, array[i]);
+          swapArrItems(array, minIndex, i);
+          await markColor_of_NormalValues(minValue, array[i]);
+      }
+      await changeColor(minValue, COLORs.SortedValue);
+  }
 }
